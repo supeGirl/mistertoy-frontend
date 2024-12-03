6859f3c5b24eacfc341045dab833879fa4bce4f1
supaGirl <kerenvasserman.bm@gmail.com>
2024-12-03 18:10:25 +0200

    Add http and toy service(work on ajax not local) services

---
+++ mistertoy-frontend/src/services/toy.service.js
@@ -0,0 +1,78 @@
+import { httpService } from './http.service.js'
+
+const BASE_URL = 'toy/'
+
+const labels = [
+  'On wheels',
+  'Box game',
+  'Art',
+  'Baby',
+  'Doll',
+  'Puzzle',
+  'Outdoor',
+  'Battery Powered',
+]
+
+
+export const toyService = {
+  query,
+  getById,
+  save,
+  remove,
+  getEmptyToy,
+  getDefaultFilter,
+  getToyLabels,
+}
+
+function query(filterBy = {}) {
+  return httpService.get(BASE_URL, filterBy)
+}
+
+function getById(toyId) {
+  return httpService.get(BASE_URL + toyId)
+}
+
+function remove(toyId) {
+  return httpService.delete(BASE_URL + toyId)
+}
+
+function save(toy) {
+  const method = toy._id ? 'put' : 'post'
+  return httpService[method](BASE_URL, toy)
+}
+
+function getDefaultFilter() {
+  return {
+    txt: '',
+    inStock: null,
+    labels: [],
+    pageIdx: 0,
+    sortBy: {
+      type: '',
+      desc: 1
+    }
+  }
+}
+
+
+function getEmptyToy() {
+  return {
+    name: '',
+    price: '',
+    labels: _getRandomLabels(),
+  }
+}
+
+function getToyLabels() {
+  return [...labels]
+}
+
+function _getRandomLabels() {
+  const labelsCopy = [...labels]
+  const randomLabels = []
+  for (let i = 0; i < 2; i++) {
+    const randomIdx = Math.floor(Math.random() * labelsCopy.length)
+    randomLabels.push(labelsCopy.splice(randomIdx, 1)[0])
+  }
+  return randomLabels
+}
